c0cfae3736877e710daa03915d064470cf9c5f71
唐肖恩 <15800690867@163.com>
2023-12-28 20:09:58 +1100

    feat: add new links

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import styles from "@/app/styles/page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Main Page</div>
+    <div>
+      <h1>Main Page</h1>
+      <ul>
+        <li>
+          <Link href={"/about"}>To about page</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
