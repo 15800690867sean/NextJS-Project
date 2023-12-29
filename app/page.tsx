16d21bcd3505fc925bc68a1a9e63ea5344e7b800
唐肖恩 <15800690867@163.com>
2023-12-29 20:00:52 +1100

    feat: render dynamic pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import styles from "@/app/styles/page.module.css";
 import Link from "next/link";
 
 export default function Home() {
+  const clients = [
+    { id: "first", name: "First" },
+    { id: "second", name: "Second" },
+  ];
+
   return (
     <div>
       <h1>Main Page</h1>
@@ -10,6 +15,13 @@ export default function Home() {
         <li>
           <Link href={"/about"}>To about page</Link>
         </li>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={`/clients/${client.name}`}
+            >{`To Client ${client.name} page`}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
