17da59a3e7ddaa5095f3ffd102730afaa33e50ac
唐肖恩 <15800690867@163.com>
2024-01-01 22:19:37 +1100

    feat: finish 404 page

---
+++ app/404.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+export default function NotFoundPage() {
+  return (
+    <div>
+        <h1>404 Not Found</h1>
+    </div>
+  )
+}
--- app/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { useParams } from "next/navigation";
-import React from "react";
-
-export default function Page() {
-  const paramsHandler = useParams();
-
-  return <div>id page, id: {paramsHandler.id}</div>;
-}
