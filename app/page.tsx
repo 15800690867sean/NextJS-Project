d28a4b08b754c250a056411fcecccb5b0e94f917
唐肖恩 <15800690867@163.com>
2024-01-03 21:09:34 +1100

    feat: fix the style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import styles from "@/app/styles/page.module.css";
-import Link from "next/link";
 import { getFeaturedEvents } from "@/mockData";
 import EventList from "../components/events/event-list";
 
@@ -8,8 +6,7 @@ export default function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className={styles.homepage}>
       <EventList items={featuredEvents}/>
     </div>
   );
