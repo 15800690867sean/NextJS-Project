6fd9c33a5e143c995b17014138efca5cb264bd3d
唐肖恩 <15800690867@163.com>
2024-01-04 21:13:46 +1100

    feat: import icons

--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -3,40 +3,46 @@
 import { EventType } from "@/mockData";
 import Button from "../ui/button";
 import React from "react";
-import styles from './event-item.module.css';
+import styles from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props: EventType) {
-  const {
-    title,
-    image,
-    date,
-    location,
-    id,
-  } = props;
+  const { title, image, date, location, id } = props;
 
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 
   const exploreLink = `/events/${id}`;
 
-  return <li className={styles.item}>
-    <img src={`/${image}`} alt={title} />
-    <div>
-      <div className={styles.content}>
-        <h2>{title}</h2>
-        <div>
-          <time>{humanReadableDate}</time>
-        </div>
-        <div>
-          <address>{location}</address>
-        </div>
-      </div>
+  return (
+    <li className={styles.item}>
+      <img src={`/${image}`} alt={title} />
       <div>
-        <Button link={exploreLink}>Explore event</Button>
+        <div className={styles.content}>
+          <h2>{title}</h2>
+          <div className={styles.rol}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={styles.rol}>
+            <AddressIcon />
+            <address>{location}</address>
+          </div>
+        </div>
+        <div>
+          <Button link={exploreLink}>
+            <div className={styles.rol}>
+              <span>Explore event</span>
+              <ArrowRightIcon />
+            </div>
+          </Button>
+        </div>
       </div>
-    </div>
-  </li>;
+    </li>
+  );
 }
