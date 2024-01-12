0676a55bb55aeeba1d748ca53a6d0a7ba16b5044
唐肖恩 <15800690867@163.com>
2024-01-12 20:43:05 +1100

    feat: use the preRendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import styles from "@/app/styles/page.module.css";
-import { getFeaturedEvents } from "@/mockData";
+// import { getFeaturedEvents } from "@/mockData";
 import EventList from "../components/events/event-list";
+import { EventType } from "@/mockData";
+import path from "path";
+import fs from 'fs/promises';
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export default async function HomePage() {
+  // const featuredEvents = getFeaturedEvents();
+  const featuredEvents: EventType[] = (await fetchData()).featuredEvents;
 
   return (
     <div className={styles.homepage}>
       <EventList items={featuredEvents}/>
     </div>
   );
+}
+
+const fetchData = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData as unknown as string);
+  return {
+      featuredEvents: data.events,
+  }
 }
