1f0ec27f921481d01ac97d53b6b26406b2b25109
唐肖恩 <15800690867@163.com>
2024-02-02 20:54:35 +1100

    feat: add feedback module

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import styles from "@/app/styles/page.module.css";
 import EventList from "@/components/events/event-list";
 import { EventType } from "@/mockData";
 import { getRealFeaturedEvents } from "@/utils/api";
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 
 // this page is using client-side data fetching, which is not good for SEO
 export default function HomePage() {
   const [events, setEvents] = useState<EventType[]>();
+  const feedbackRef = useRef() as RefObject<HTMLInputElement>;
 
   useEffect(() => {
     getRealFeaturedEvents().then((res) => {
@@ -16,12 +17,29 @@ export default function HomePage() {
     });
   }, []);
 
+  const handleFeedback = () => {
+    const feedback = feedbackRef.current?.value;
+    console.log(feedback);
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify({
+        content: feedback,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+  }
+
   if (!events) {
     return <p>Loading...</p>
   }
 
   return (
     <div className={styles.homepage}>
+      <label htmlFor="feedback">Feedback:</label>
+      <input type="text" id="feedback" ref={feedbackRef}/>
+      <button onClick={handleFeedback}>Submit Feedback</button>
       <EventList items={events}/>
     </div>
   );
--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,5 +1,28 @@
-export default function handler(req: unknown, res: any) {
-    res.status(200).json({
-        message: 'this works',
+import fs from "fs";
+import path from "path";
+
+export default function handler(
+  req: Record<string, any>,
+  res: Record<string, any>
+) {
+  const filePath = path.join(process.cwd(), "data", "feedback.json");
+
+  if (req.method === "POST") {
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData as unknown as string);
+    const content = req.body.content;
+    data.push(content);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({
+      message: "feedback saved",
+      data,
     });
-};
+  } else {
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData as unknown as string);
+    res.status(200).json({
+      message: "this works",
+      data,
+    });
+  }
+}
