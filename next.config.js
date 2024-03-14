80e68751921740b58b52d592186373cd7128611c
Sean Tang <tangxiaoensean@gmail.com>
2024-03-15 00:47:49 +1100

    feat: update the portfolio

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <=== enables static exports
+//   output: "export", // <=== enables static exports
   reactStrictMode: true,
-  basePath: "/nextjs-app",
   images: {
     unoptimized: true
 }
